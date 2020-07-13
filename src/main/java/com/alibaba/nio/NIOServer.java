70afb2318173819ef4f25152cfae8614add7316c
e-shenbin1 <e-shenbin1@geely.com>
2020-07-13 21:51:19 +0800

    11

--- src/main/java/com/alibaba/nio/NIOServer.java
+++ src/main/java/com/alibaba/nio/NIOServer.java
@@ -2,10 +2,8 @@ package com.alibaba.nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -35,9 +33,24 @@ public class NIOServer {
                 SelectionKey key = keyIterator.next();
                 //根据key对应的通道发送的事件做对应的处理
                 if(key.isAcceptable()){
-                    SocketChannel accept = serverSocketChannel.accept();
+                    //讲改客户端生成一个socketchannel
+                    SocketChannel socketChannel = serverSocketChannel.accept();
                     System.out.println("客户端连接成功");
+                    //将sockechannel设置为非阻塞
+                    socketChannel.configureBlocking(false);
+                    //将socketchannel注册到selector，关注事件为OP_READ，同事给socketchannel
+                    //关联一个buffer
+                    socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
                 }
+                if(key.isReadable()){//如果发生OP_READ
+                    //通过key反向获取到对应channel
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    channel.read(buffer);
+                    System.out.println("form 客户端"+new String(buffer.array()));
+                }
+                //手动从集合中移动当前的selectorKey，防止重复操作
+                keyIterator.remove();
             }
         }
     }
